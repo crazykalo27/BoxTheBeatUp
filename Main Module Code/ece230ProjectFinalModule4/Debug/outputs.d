841056603a7b00d69d4cc4fda4e09f47db87ba8f
Selby <selbykj@rose-hulman.edu>
2025-04-10 10:15:26 -0400

    Added Main Module Code - TI and ESP32

---
+++ Main Module Code/ece230ProjectFinalModule4/Debug/outputs.d
@@ -0,0 +1,78 @@
+# FIXED
+
+outputs.obj: ../outputs.c
+outputs.obj: ../outputs.h
+outputs.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/msp.h
+outputs.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/msp432p4111.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+outputs.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/msp_compatibility.h
+outputs.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+outputs.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+outputs.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+outputs.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/system_msp432p4111.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/math.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_defs.h
+outputs.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_limits.h
+
+../outputs.c:
+
+../outputs.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/msp.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/msp432p4111.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/msp_compatibility.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/CMSIS/core_cm4.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/system_msp432p4111.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/math.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_defs.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_limits.h:
+
---
+++ Main Module Code/ece230ProjectFinalModule4/Debug/subdir_vars.mk
@@ -0,0 +1,59 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p4111.cmd 
+
+C_SRCS += \
+../FSRADC.c \
+../csHFXT.c \
+../ece230ProjectFinalModule4-1.c \
+../outputs.c \
+../startup_msp432p4111_ccs.c \
+../system_msp432p4111.c 
+
+C_DEPS += \
+./FSRADC.d \
+./csHFXT.d \
+./ece230ProjectFinalModule4-1.d \
+./outputs.d \
+./startup_msp432p4111_ccs.d \
+./system_msp432p4111.d 
+
+OBJS += \
+./FSRADC.obj \
+./csHFXT.obj \
+./ece230ProjectFinalModule4-1.obj \
+./outputs.obj \
+./startup_msp432p4111_ccs.obj \
+./system_msp432p4111.obj 
+
+OBJS__QUOTED += \
+"FSRADC.obj" \
+"csHFXT.obj" \
+"ece230ProjectFinalModule4-1.obj" \
+"outputs.obj" \
+"startup_msp432p4111_ccs.obj" \
+"system_msp432p4111.obj" 
+
+C_DEPS__QUOTED += \
+"FSRADC.d" \
+"csHFXT.d" \
+"ece230ProjectFinalModule4-1.d" \
+"outputs.d" \
+"startup_msp432p4111_ccs.d" \
+"system_msp432p4111.d" 
+
+C_SRCS__QUOTED += \
+"../FSRADC.c" \
+"../csHFXT.c" \
+"../ece230ProjectFinalModule4-1.c" \
+"../outputs.c" \
+"../startup_msp432p4111_ccs.c" \
+"../system_msp432p4111.c" 
+
+
